517c30e5cc6e0637f2d73f925b2fd1f0f8e1fc65
sarbadaj <sarbadaj@yahoo.com>
2014-12-15 20:23:26 +0545

    modified for establish relationship between user and write board model

--- app/models/writeboard.rb
+++ app/models/writeboard.rb
@@ -1,2 +1,6 @@
 class Writeboard < ActiveRecord::Base
+	has_and_belongs_to_many :users
+
+	validates :title, presence: true
+	validates :body, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,10 @@ Rails.application.routes.draw do
   resources :users
   resources :password_resets
 
+  resources :users do
+    resources :writeboards
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
