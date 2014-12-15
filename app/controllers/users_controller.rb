81aed331bd9c83650d7b98a9b568f8e83d67abfb
sarbadaj <sarbadaj@yahoo.com>
2014-12-15 12:09:10 +0545

    modified for users signup and signin

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,36 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
+
+  def create
+    if params[:commit] == 'Signup'
+      @user = User.new(user_params)
+      if @user.save
+        redirect_to root_url, :notice => "Signed up!"
+      else
+        render "new"
+      end
+    elsif params[:commit] == 'Signin'
+      user = User.authenticate(params[:user][:email], params[:user][:password])
+      if user
+        session[:user_id] = user.id
+        redirect_to root_url, :notice => "Logged in!"
+      else
+        flash.now.alert = "Invalid email or password"
+        redirect_to root_url
+      end
+    end
+      
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_url, :notice => "Logged out!"
+  end
+  
+  private
+    def user_params
+      params.require(:user).permit(:email, :password)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   get 'users/new'
 
+  get "log_out" => "users#destroy", :as => "log_out"
+  get "log_in" => "users#new", :as => "log_in"
+  get "sign_up" => "users#new", :as => "sign_up"
+  root :to => "users#new"
+  resources :users
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
